4883a375628bcbfae0fb9020146829f8041cf969
virtualblakewillis <bewillis101@gmail.com>
2023-08-01 14:08:59 -0500

    I have Tablesgit add .git add .

--- sql/database_init.sql
+++ sql/database_init.sql
@@ -1,45 +1,41 @@
-CREATE DATABASE joy_of_painting IF NOT EXISTS;
+CREATE DATABASE  IF NOT EXISTS joy_of_painting;
 USE joy_of_painting;
 
--- create tables: episodes, colors, subject_matters
-CREATE TABLE episodes IF NOT EXISTS (
+/* create tables: episodes, colors, subject_matters */
+CREATE TABLE IF NOT EXISTS episodes (
     episode_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     air_date DATE NOT NULL,
     episode_number INT NOT NULL,
     season_number INT NOT NULL,
     painting_img_src VARCHAR(255) NOT NULL,
-    painting_yt_src VARCHAR(255) NOT NULL,
+    painting_yt_src VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE colors IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS colors (
     color_id INT PRIMARY KEY AUTO_INCREMENT,
     color_name VARCHAR(255) NOT NULL,
-    color_hex VARCHAR(255) NOT NULL,
+    color_hex VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE subject_matters IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS subject_matters(
     subject_matter_id INT PRIMARY KEY AUTO_INCREMENT,
-    subject_matter_name VARCHAR(255) NOT NULL,
+    subject_matter_name VARCHAR(255) NOT NULL
 );
 
--- create join tables: episode_colors, episode_subject_matters
-CREATE TABLE episode_colors IF NOT EXISTS (
+/* create join tables: episode_colors, episode_subject_matters */
+CREATE TABLE IF NOT EXISTS episode_colors (
     episode_color_id INT PRIMARY KEY AUTO_INCREMENT,
     episode_id INT NOT NULL,
     color_id INT NOT NULL,
     FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
-    FOREIGN KEY (color_id) REFERENCES colors(color_id),
+    FOREIGN KEY (color_id) REFERENCES colors(color_id)
 );
 
-CREATE TABLE episode_subject_matters IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS episode_subject_matters (
     episode_subject_matter_id INT PRIMARY KEY AUTO_INCREMENT,
     episode_id INT NOT NULL,
     subject_matter_id INT NOT NULL,
     FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
-    FOREIGN KEY (subject_matter_id) REFERENCES subject_matters(subject_matter_id),
+    FOREIGN KEY (subject_matter_id) REFERENCES subject_matters(subject_matter_id)
 );
-
-SHOW TABLES FROM joy_of_painting;
-
-
---
+++ src/app.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql');
+const fs = require('fs');
+// Replace these with your MySQL credentials
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  multipleStatements: true
+});
+
+// Read the SQL script file
+const scriptFilePath = './sql/database_init.sql';
+const sqlScript = fs.readFileSync(scriptFilePath, 'utf8');
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+
+  // Execute the SQL script
+  connection.query(sqlScript, (err, results, fields) => {
+    if (err) {
+      console.error('Error executing SQL script:', err);
+      return;
+    }
+
+    console.log('SQL script executed successfully.');
+  });
+
+  // Close the database connection
+  connection.end((err) => {
+    if (err) {
+      console.error('Error closing MySQL connection:', err);
+      return;
+    }
+    console.log('MySQL connection closed.');
+  });
+});
+
